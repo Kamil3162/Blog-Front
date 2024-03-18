6531722ea4ba3b117962c9cf65b64cea386ec5a7
unknown <kamilholub_1@wp.pl>
2024-03-18 20:36:47 +0100

    Add hooks to API

--- blog-front/src/services/post_service.js
+++ blog-front/src/services/post_service.js
@@ -15,9 +15,6 @@ export const postCreate = (title, category, content, photo) => {
     axios.post(API_URL + '/post-create/',formData,{
         withCredentials: true, // This enables cross-origin requests to include cookies
         headers: {
-            // Inform the server about the form-data content type
-            // This line might be optional as browsers usually set the correct content type for FormData,
-            // including the boundary parameter.
             'Content-Type': 'multipart/form-data',
         },
     }).then(response => {
@@ -27,18 +24,58 @@ export const postCreate = (title, category, content, photo) => {
     })
 }
 
-export const postUpdate = () =>{
+export const postUpdate = (post_id, title, content) =>{
+    const formData = new FormData();
+
+    formData.append('content', content);
+    formData.append('title', title);
+
+    axios.put(API_URL + `/post-update/{post_id}`, formData,{
+        withCredentials: true, // This enables cross-origin requests to include cookies
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    }).then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    })
+}
+
+export const postDelete = (post_id) =>{
+
+    axios.delete(API_URL + `/post-update/{post_id}`,{
+        withCredentials: true, // This enables cross-origin requests to include cookies
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    }).then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    })
 
 }
 
-export const postDelete = () =>{
-
-}
-
-export const postDetail = () =>{
-
+export const postDetail = (post_id) =>{
+    axios.put(API_URL + `/post/{post_id}`,{
+        withCredentials: true, // This enables cross-origin requests to include cookies
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    }).then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    })
 }
 
 export const fetchPosts = () => {
+    axios.get(API_URL + '/posts',{
 
+    }).then(response =>{
+        console.log(response);
+    }).catch(error =>{
+        console.log(error);
+    })
 }
