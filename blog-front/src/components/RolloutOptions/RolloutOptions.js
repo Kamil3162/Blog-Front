9d38bee0236dc773e3081efa2a379ec385d82c22
Kamil Holub <kamilholub_1@wp.pl>
2024-10-22 23:18:25 +0200

    Fix rollout options component

--- blog-front/src/components/RolloutOptions/RolloutOptions.js
+++ blog-front/src/components/RolloutOptions/RolloutOptions.js
@@ -13,20 +13,27 @@ function RolloutOptions(){
 
     const { width } = useContext(WindowSizeContext);
 
+    console.log(isNavVisible);
+    console.log(width);
+    console.log(width <= 500 && isNavVisible);
     const changeClickNavVisible = (event) => {
         setIsNavVisible(false);
+        console.log("changeclicknav visible");
     }
 
+    console.log("test message");
+
     return (
-        <NavigationContainer showOnSite={width <= 500 && isNavVisible}>
-            {navigationItems.map((item, index) => (
-                <NavigationElement key={index}>
-                    <Link to={item.href} onClick={changeClickNavVisible}>
-                        {item.label}
-                    </Link>
-                </NavigationElement>
-            ))}
-        </NavigationContainer>
+         width <= 500 && isNavVisible &&
+                <NavigationContainer showOnSite={width <= 500 && isNavVisible}>
+                    {navigationItems.map((item, index) => (
+                        <NavigationElement key={index}>
+                            <Link to={item.href} onClick={changeClickNavVisible}>
+                                {item.label}
+                            </Link>
+                        </NavigationElement>
+                    ))}
+                </NavigationContainer>
     );
 }
 
